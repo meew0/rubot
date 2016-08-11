1eea6dbe9449bd595d53be2766d64ba0b82a7614
meew0 <blactbt@live.de>
2016-08-11 15:40:28 +0200

    Implement the BetsFile class to represent the rubot-bets.json file

--- handlers/status.rb
+++ handlers/status.rb
@@ -1,4 +1,41 @@
 # frozen_string_literal: true
 
+require 'json'
+require 'singleton'
+
 module RubotHandlers::Status
+  class BetsFile
+    include Singleton
+
+    FILE_PATH = 'rubot-bets.json'
+
+    attr_reader :balances
+    attr_reader :chances
+
+    def initialize
+      read
+    end
+
+    def read
+      obj = JSON.parse(File.read(FILE_PATH))
+
+      @enabled_repos = obj['enabled_repos']
+      @balances = obj['balances']
+      @chances = obj['chances']
+    end
+
+    def write
+      obj = {
+        enabled_repos: @enabled_repos,
+        balances: @balances,
+        chances: @chances
+      }
+
+      File.write(FILE_PATH, obj.to_json)
+    end
+
+    def enabled?(repo)
+      @enabled_repos.include? repo
+    end
+  end
 end
