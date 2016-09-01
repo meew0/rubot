5cf812ea9e5e9bc8d8e09617a7d72cbcda2e242f
acdenisSK <acdenisSK@outlook.com>
2016-09-01 15:19:02 +0200

    ignore deleted and edited comments

--- handlers/issue_comment.rb
+++ handlers/issue_comment.rb
@@ -1,5 +1,6 @@
 module RubotHandlers::IssueComment
   def self.handle(payload)
+    return if payload.action == "edited" or payload.action == "deleted" 
     %(commented on issue #{payload.tiny_issue}
 <#{payload['comment']['html_url']}>)
   end
